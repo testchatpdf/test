248ec68e05c134e9bef078d71ab5445b5916d350
Lalo <deverwongsawat@gmail.com>
2025-07-29 15:27:16 +0700

    Thông điệp commit của bạn

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,7 @@
 import requests
 
 # Sử dụng API Key trực tiếp (thay bằng API Key thực tế của bạn)
-API_KEY = "sec_9Z9B3PU67Id21XLmCCfQ4G4sTpm49iPN"
+API_KEY = "sec_Cc2RpBY3WeEbjtRbBTrlomxufxHq5nfz"
 
 # Hàm gửi câu hỏi tới ChatPDF với sourceId cố định
 def ask_question(source_id, question):
